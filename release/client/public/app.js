5f28bfd0230eafc641317a18197a921210da5194
Nathan <nazzanuk@gmail.com>
2015-10-19 16:53:56 +0100

    :P

--- release/client/public/app.js
+++ release/client/public/app.js
@@ -19,34 +19,6 @@ app.directive('ngEnter', function () {
         });
     };
 });
-app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
-
-    var resolve = {
-        timeout: function timeout($timeout) {
-            $('[screen]').removeClass('active');
-            //$('.loading-logo').addClass('active');
-            return $timeout(300);
-        }
-    };
-
-    // For any unmatched url, redirect to /
-    $urlRouterProvider.otherwise("/");
-
-    // Now set up the states
-    $stateProvider.state('home', {
-        url: "/",
-        templateUrl: "home-screen.html",
-        controller: "HomeCtrl",
-        resolve: resolve
-    }).state('post', {
-        url: "/post/:id",
-        templateUrl: "post-screen.html",
-        controller: "PostCtrl",
-        resolve: resolve
-    });
-
-    $locationProvider.html5Mode(true);
-});
 'use strict';
 
 app.factory('Post', function ($timeout, $rootScope) {
@@ -94,6 +66,34 @@ app.factory('Post', function ($timeout, $rootScope) {
     return Post;
 });
 
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+
+    var resolve = {
+        timeout: function timeout($timeout) {
+            $('[screen]').removeClass('active');
+            //$('.loading-logo').addClass('active');
+            return $timeout(300);
+        }
+    };
+
+    // For any unmatched url, redirect to /
+    $urlRouterProvider.otherwise("/");
+
+    // Now set up the states
+    $stateProvider.state('home', {
+        url: "/",
+        templateUrl: "home-screen.html",
+        controller: "HomeCtrl",
+        resolve: resolve
+    }).state('post', {
+        url: "/post/:id",
+        templateUrl: "post-screen.html",
+        controller: "PostCtrl",
+        resolve: resolve
+    });
+
+    $locationProvider.html5Mode(true);
+});
 app.controller('ScreenCtrl', function ($element, $timeout, State, $state) {
 
     var init = function init() {
@@ -269,12 +269,10 @@ app.directive('about', function ($timeout) {
 
 'use strict';
 
-app.directive('heading', function () {
+app.directive('header', function (State) {
     return {
-        templateUrl: 'heading.html',
-        scope: {
-            text: '='
-        },
+        templateUrl: 'header.html',
+        scope: {},
 
         link: function link(scope, element, attrs) {
 
@@ -282,7 +280,10 @@ app.directive('heading', function () {
 
             init();
 
-            scope = _.assign(scope, {});
+            scope = _.assign(scope, {
+                isMenuVisible: State.isMenuVisible,
+                toggleMenu: State.toggleMenu
+            });
         }
     };
 });
@@ -329,6 +330,26 @@ app.directive('hero', function (API, Post, $timeout) {
 
 'use strict';
 
+app.directive('heading', function () {
+    return {
+        templateUrl: 'heading.html',
+        scope: {
+            text: '='
+        },
+
+        link: function link(scope, element, attrs) {
+
+            var init = function init() {};
+
+            init();
+
+            scope = _.assign(scope, {});
+        }
+    };
+});
+
+'use strict';
+
 app.directive('imageItem', function () {
     return {
         templateUrl: 'image-item.html',
@@ -365,32 +386,6 @@ app.directive('menuOverlay', function ($timeout, State) {
             scope = _.assign(scope, {
                 isMenuVisible: State.isMenuVisible,
                 toggleMenu: State.toggleMenu
-            });
-        }
-    };
-});
-
-'use strict';
-
-app.directive('paragraph', function ($sce) {
-    return {
-        templateUrl: 'paragraph.html',
-        scope: {
-            text: '='
-        },
-
-        link: function link(scope, element, attrs) {
-
-            var getText = function getText() {
-                return $sce.trustAsHtml(scope.text);
-            };
-
-            var init = function init() {};
-
-            init();
-
-            scope = _.assign(scope, {
-                getText: getText
             });
         }
     };
@@ -445,6 +440,32 @@ app.directive('preview', function (API, Post, $timeout) {
 
 'use strict';
 
+app.directive('paragraph', function ($sce) {
+    return {
+        templateUrl: 'paragraph.html',
+        scope: {
+            text: '='
+        },
+
+        link: function link(scope, element, attrs) {
+
+            var getText = function getText() {
+                return $sce.trustAsHtml(scope.text);
+            };
+
+            var init = function init() {};
+
+            init();
+
+            scope = _.assign(scope, {
+                getText: getText
+            });
+        }
+    };
+});
+
+'use strict';
+
 app.directive('quoteItem', function () {
     return {
         templateUrl: 'quote-item.html',
@@ -490,27 +511,6 @@ app.directive('vid', function () {
                 play: play,
                 pause: pause
 
-            });
-        }
-    };
-});
-
-'use strict';
-
-app.directive('header', function (State) {
-    return {
-        templateUrl: 'header.html',
-        scope: {},
-
-        link: function link(scope, element, attrs) {
-
-            var init = function init() {};
-
-            init();
-
-            scope = _.assign(scope, {
-                isMenuVisible: State.isMenuVisible,
-                toggleMenu: State.toggleMenu
             });
         }
     };

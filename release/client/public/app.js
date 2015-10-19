050e12cce7488af8b08e38f93dceef68a3185975
Nathan <nazzanuk@gmail.com>
2015-10-19 22:02:32 +0100

    :P

--- release/client/public/app.js
+++ release/client/public/app.js
@@ -269,6 +269,27 @@ app.directive('about', function ($timeout) {
 
 'use strict';
 
+app.directive('header', function (State) {
+    return {
+        templateUrl: 'header.html',
+        scope: {},
+
+        link: function link(scope, element, attrs) {
+
+            var init = function init() {};
+
+            init();
+
+            scope = _.assign(scope, {
+                isMenuVisible: State.isMenuVisible,
+                toggleMenu: State.toggleMenu
+            });
+        }
+    };
+});
+
+'use strict';
+
 app.directive('heading', function () {
     return {
         templateUrl: 'heading.html',
@@ -398,47 +419,6 @@ app.directive('paragraph', function ($sce) {
 
 'use strict';
 
-app.directive('header', function (State) {
-    return {
-        templateUrl: 'header.html',
-        scope: {},
-
-        link: function link(scope, element, attrs) {
-
-            var init = function init() {};
-
-            init();
-
-            scope = _.assign(scope, {
-                isMenuVisible: State.isMenuVisible,
-                toggleMenu: State.toggleMenu
-            });
-        }
-    };
-});
-
-'use strict';
-
-app.directive('quoteItem', function () {
-    return {
-        templateUrl: 'quote-item.html',
-        scope: {
-            content: '='
-        },
-
-        link: function link(scope, element, attrs) {
-
-            var init = function init() {};
-
-            init();
-
-            scope = _.assign(scope, {});
-        }
-    };
-});
-
-'use strict';
-
 app.directive('preview', function (API, Post, $timeout) {
     return {
         templateUrl: 'preview.html',
@@ -480,6 +460,26 @@ app.directive('preview', function (API, Post, $timeout) {
 
             scope.getReverseClass = getReverseClass;
             scope.getPost = getPost;
+        }
+    };
+});
+
+'use strict';
+
+app.directive('quoteItem', function () {
+    return {
+        templateUrl: 'quote-item.html',
+        scope: {
+            content: '='
+        },
+
+        link: function link(scope, element, attrs) {
+
+            var init = function init() {};
+
+            init();
+
+            scope = _.assign(scope, {});
         }
     };
 });
